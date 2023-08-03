6e91a2c6ecf65df8ab581c3dfdb4a4650f86d24a
Patrick Calderon <patriciocc98@gmail.com>
2023-08-03 12:31:28 -0500

    Finished my module on storing an entity object using Core Data save() feature. I created an entity that contained string, binary data, integer 32 and UUID attributes. I also learnt about the save function that requires a do and a try catch in orderto handle any errors when commiting unsaved changes to the parent store, in this case the appDelegate persistentContainer.

--- The Complete iOS 16 : iOS 15 Developer Course - and SwiftUI!/ArtBookProject/ArtBookProject/DetailsViewController.swift
+++ The Complete iOS 16 : iOS 15 Developer Course - and SwiftUI!/ArtBookProject/ArtBookProject/DetailsViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import PhotosUI
+import CoreData
 
 class DetailsViewController: UIViewController, PHPickerViewControllerDelegate {
     
@@ -80,7 +81,33 @@ class DetailsViewController: UIViewController, PHPickerViewControllerDelegate {
     }
 
     @IBAction func saveButtonClicked(_ sender: Any) {
-        print("test")
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newPainting = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
+        
+        // Attributes
+        
+        newPainting.setValue(nameTextField.text!, forKey: "name")
+        newPainting.setValue(artistTextField.text!, forKey: "artist")
+        
+        if let year = Int(yearTextField.text!) {
+            newPainting.setValue(year, forKey: "year")
+        }
+        
+        newPainting.setValue(UUID(), forKey: "id")
+        
+        let data = imageView.image?.jpegData(compressionQuality: 0.5)
+        
+        newPainting.setValue(data, forKey: "image")
+        
+        do {
+            try context.save()
+            print("success")
+        } catch {
+            print("error")
+        }
     }
     
     
