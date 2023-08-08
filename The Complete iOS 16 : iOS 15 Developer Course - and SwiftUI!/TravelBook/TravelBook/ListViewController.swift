91e50bc95fce20992d52c8a20cde2b7a0460a0d8
Patrick Calderon <patriciocc98@gmail.com>
2023-08-08 18:27:28 -0500

    Fetched data from CoreData to show it on a TableView.

--- The Complete iOS 16 : iOS 15 Developer Course - and SwiftUI!/TravelBook/TravelBook/ListViewController.swift
+++ The Complete iOS 16 : iOS 15 Developer Course - and SwiftUI!/TravelBook/TravelBook/ListViewController.swift
@@ -6,13 +6,14 @@
 //
 
 import UIKit
+import CoreData
 
 class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-  
-    
 
     @IBOutlet weak var tableView: UITableView!
+    var titleArray = [String]()
+    var idArray = [UUID]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +22,41 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        getData()
+    }
+    
+    func getData() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(request)
+            if results.count > 0 {
+                
+                // Prevent data duplication
+                self.idArray.removeAll(keepingCapacity: false)
+                self.titleArray.removeAll(keepingCapacity: false)
+                
+                for result in results as! [NSManagedObject]{
+                    
+                    if let title = result.value(forKey: "title") as? String {
+                        titleArray.append(title)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID {
+                        self.idArray.append(id)
+                    }
+                    
+                }
+            }
+        } catch {
+            
+        }
+            
+        
     }
     
     @objc func addButtonClicked() {
@@ -29,13 +65,13 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return titleArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         var content = cell.defaultContentConfiguration()
-        content.text = "test"
+        content.text = titleArray[indexPath.row]
         cell.contentConfiguration = content
         return cell
         
