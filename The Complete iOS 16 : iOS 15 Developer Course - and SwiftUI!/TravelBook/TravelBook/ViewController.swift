87be0cd9ad6492f18377b80a5e43d29406773c15
Patrick Calderon <patriciocc98@gmail.com>
2023-08-08 14:39:56 -0500

    Added a key value in info.plist file that describes the message the user receives when asked for permission to use location services. I also implemented the function locationManagerDidChangeAuthorization from Apple's documentation which allows me to check the level of authorization the app has, if it's authorized then it enables location features through another function, if it's not determined then it prompts the user with a request to use location services. Then in the didUpdateLocations function inherited from the CLLocationManagerDelegate we setup the current location of the user, the span for the map and the region.

--- The Complete iOS 16 : iOS 15 Developer Course - and SwiftUI!/TravelBook/TravelBook/ViewController.swift
+++ The Complete iOS 16 : iOS 15 Developer Course - and SwiftUI!/TravelBook/TravelBook/ViewController.swift
@@ -7,17 +7,60 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
     @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManagerDidChangeAuthorization(locationManager)
+        
+    }
+    
+    
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+            case .authorizedWhenInUse:  // Location services are available.
+                enableLocationFeatures()
+                break
+
+            case .restricted, .denied:  // Location services currently unavailable.
+                //disableLocationFeatures()
+                break
+
+            case .notDetermined:        // Authorization not determined yet.
+                manager.requestWhenInUseAuthorization()
+                break
+
+            default:
+                break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let currentLocation = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+            let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+            let region = MKCoordinateRegion(center: currentLocation, span: span)
+            mapView.setRegion(region, animated: true)
+            
+        }
+        
     }
 
-
+    func enableLocationFeatures() {
+        locationManager.startUpdatingLocation()
+    }
 }
+
+
 
