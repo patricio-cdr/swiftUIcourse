132105dc602cc3858f6bd62853854edaccef32fc
Patrick Calderon <patriciocc98@gmail.com>
2023-08-23 10:10:41 -0500

    Used traitCollection to obtain userInterface style which helped me determine if the user's iPhone is in light or dark mode.

--- The Complete iOS 16 : iOS 15 Developer Course - and SwiftUI!/DarkModeApp/DarkModeApp/ViewController.swift
+++ The Complete iOS 16 : iOS 15 Developer Course - and SwiftUI!/DarkModeApp/DarkModeApp/ViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var changeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        
+        if userInterfaceStyle == .dark {
+            changeButton.tintColor = UIColor.white
+            
+        } else {
+            changeButton.tintColor = UIColor.blue
+        }
     }
 
 
