1ee89d4b7f257b81c456dc2dd89f7520372d8f2d
Patrick Calderon <patriciocc98@gmail.com>
2023-08-28 10:07:54 -0500

    I started to implement the function that consumes an API by first creating a request and session.

--- The Complete iOS 16 : iOS 15 Developer Course - and SwiftUI!/CurrencyConverter/CurrencyConverter/ViewController.swift
+++ The Complete iOS 16 : iOS 15 Developer Course - and SwiftUI!/CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -23,6 +23,42 @@ class ViewController: UIViewController {
     }
 
     @IBAction func getRatesClicked(_ sender: Any) {
+        
+        // This are the steps we need to follow in order to consume data from an API
+        // 1) Request & Session
+        // 2) Response & Data
+        // 3) Parsing & JSON Serialization
+        
+        
+        // 1) Request & Session
+        if let url = URL(string: "http://data.fixer.io/api/latest?access_key=f9a7c915459a471887f55345ac7a57e7") {
+            
+            let session = URLSession.shared
+            // This is a closure
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                    let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+                    alert.addAction(okButton)
+                    
+                    // Since we are in a closure, in order to present the alert we need to refer to self
+                    self.present(alert, animated: true, completion: nil)
+                } else {
+                    
+                    // 2) Response & Data
+                    if data != nil {
+                        
+                    }
+                }
+            }
+        }
+        
+        
+        // 2) Response & Data
+        // 3) Parsing & JSON Serialization
+        
+        
+        
     }
     
 }
